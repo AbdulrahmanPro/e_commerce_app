b9b97a8d94f0c19356e6e9dfff438a1669325ece
AbdulrahmanPro <abdulrahmanpro213@gmail.com>
2026-02-09 11:57:46 +0300

    edit product item to use bloc state managment

--- lib/views/widgets/product_item.dart
+++ lib/views/widgets/product_item.dart
@@ -1,6 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:e_commerce/models/product_item_model.dart';
+import 'package:e_commerce/view_model/cubit/home_cubit_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ProductItem extends StatelessWidget {
   final ProductItemModel product;
